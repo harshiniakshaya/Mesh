0300b8698a90ced938d9636428baec52a93846ea
harshiniakshaya <harshiniakshaya@gmail.com>
2025-07-20 15:06:13 +0530

    DB Connected

---
+++ Backend/src/main/java/com/mesh/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.mesh.controller;
+import com.mesh.model.User;
+import com.mesh.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return userRepository.findByEmail(email).orElse(null);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userRepository.deleteById(id);
+    }
+}
