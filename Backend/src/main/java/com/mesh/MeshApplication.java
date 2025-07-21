b5aa17f2424ec933fd1e2f498cbe74f78eec5071
Gopi04-github <rajagokul231@gmail.com>
2025-07-21 17:33:08 +0530

    added login signup

--- Backend/src/main/java/com/mesh/MeshApplication.java
+++ Backend/src/main/java/com/mesh/MeshApplication.java
@@ -2,7 +2,6 @@ package com.mesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MeshApplication {
     public static void main(String[] args) {
--- Backend/src/main/java/com/mesh/controller/UserController.java
+++ Backend/src/main/java/com/mesh/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.mesh.controller;
+
 import com.mesh.model.User;
 import com.mesh.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -22,9 +25,31 @@ public class UserController {
         return userRepository.findByEmail(email).orElse(null);
     }
 
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    @PostMapping("/signup")   // Separate endpoint for students
+    public String studentSignup(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return "Email already registered";
+        }
+
+        user.setRole("student");
+        user.setRegisteredAt(LocalDateTime.now());
+        userRepository.save(user);
+
+        return "Student registered successfully";
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        User existingUser = userRepository.findByEmail(user.getEmail()).orElse(null);
+
+        if (existingUser == null) {
+            return "Email not found";
+        }
+
+        if (existingUser.getPassword().equals(user.getPassword())) {
+            return "Login successful as "+existingUser.getRole();
+        } else {
+            return "Incorrect password";
+        }
     }
 
     @DeleteMapping("/{id}")
